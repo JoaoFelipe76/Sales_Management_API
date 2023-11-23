c72b7b5e867164291dbe3359baa12c396a2307c3
Jf.Lorenzon() <129761507+JoaoFelipe76@users.noreply.github.com>
2023-11-22 21:15:39 -0300

    Update V01__criar_e_registrar_categorias.sql

--- src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -3,8 +3,8 @@ CREATE TABLE categoria (
 	nome VARCHAR(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
 
-INSERT INTO categoria (nome) VALUES ('Acessórios Automotivos');
-INSERT INTO categoria (nome) VALUES ('Som e Áudio');
-INSERT INTO categoria (nome) VALUES ('Utilidades para Veículos');
-INSERT INTO categoria (nome) VALUES ('Peças Eletrônicas');
-INSERT INTO categoria (nome) VALUES ('Peças de Reposição');
+INSERT INTO categoria (nome) VALUES ('Injetores & Drivers');
+INSERT INTO categoria (nome) VALUES ('Ignição');
+INSERT INTO categoria (nome) VALUES ('Volantes');
+INSERT INTO categoria (nome) VALUES ('Vestuário e Acessórios');
+INSERT INTO categoria (nome) VALUES ('Peças de carro em geral');
